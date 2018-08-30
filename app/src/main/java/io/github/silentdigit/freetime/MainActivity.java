90f535be513d32ec77133ad251a44e7ec5bd31ae
silentdigit <silentdigit@users.noreply.github.com>
2018-08-30 21:30:05 +1000

    Major changes to project. MainActivity close to required functionality for MVP.

--- app/src/main/java/io/github/silentdigit/freetime/MainActivity.java
+++ app/src/main/java/io/github/silentdigit/freetime/MainActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -13,14 +15,21 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     LocationManager locationManager;
     LocationListener locationListener;
     Location currentLocation;
+    Location destination;
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -45,6 +54,32 @@ public class MainActivity extends AppCompatActivity {
             public void onLocationChanged(Location location) {
                 // If user moves more than a pre-determined threshold value, this method is called
                 currentLocation = location;
+
+                String latLong = String.format(Locale.getDefault(), "%.3f, %.3f", location.getLatitude(), location.getLongitude());
+                TextView locationText = findViewById(R.id.locationTextView);
+                locationText.setText(latLong);
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+                try {
+                    List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+                    if (listAddresses != null && listAddresses.size() > 0) {
+                        Address currentAddress = listAddresses.get(0);
+                        String addString = currentAddress.getAddressLine(0) + "\n" + locationText.getText();
+                        locationText.setText(addString);
+
+                        Log.i("PlaceInfo", currentAddress.toString());
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                if (destination != null) {
+                    updateDistance();
+                }
+
                 Log.i("Location: ", location.toString()); // Used for debugging
             }
 
@@ -71,19 +106,33 @@ public class MainActivity extends AppCompatActivity {
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
         }
 
+        // KeyListener for destinationEditText
+        EditText destinationEditText = findViewById(R.id.destinationEditText);
+
+        destinationEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if (view.getId() == R.id.destinationEditText && keyEvent.getAction() == KeyEvent.ACTION_DOWN &&
+                        i == KeyEvent.KEYCODE_ENTER) {
+                    setDestination(view);
+
+                    toastMessage();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        // Sets default text for location TextViews
         TextView locationText = findViewById(R.id.locationTextView);
         String searchString = "Searching...";
         locationText.setText(searchString);
-    }
 
-    // Used to update the location data on screen upon pressing the update location button
-    public void updateLocation(View view) {
-        TextView locationText = findViewById(R.id.locationTextView);
-        String latLong = "Searching";
-        if (currentLocation != null) {
-            latLong = String.valueOf(currentLocation.getLatitude()).concat(", ").concat(String.valueOf(currentLocation.getLongitude()));
-        }
-        locationText.setText(latLong);
+        TextView destinationText = findViewById(R.id.destinationTextView);
+        destinationText.setText("");
+
+        TextView distanceText = findViewById(R.id.distanceTextView);
+        distanceText.setText("");
     }
 
     // Method used to transition to MapActivity
@@ -93,6 +142,46 @@ public class MainActivity extends AppCompatActivity {
         startActivity(mapIntent);
     }
 
+    public void setDestination(View view) {
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+        try {
+            EditText destText = findViewById(R.id.destinationEditText);
+            List<Address> destAddresses = geocoder.getFromLocationName(destText.getText().toString(), 1);
+
+            if (destAddresses != null && destAddresses.size() > 0) {
+                Address destAddress = destAddresses.get(0);
+                String destString = destAddress.getAddressLine(0) + "\n" +
+                        String.format(Locale.getDefault(), "%.3f, %.3f", destAddress.getLatitude(),destAddress.getLongitude());
+
+
+                TextView destTextView = findViewById(R.id.destinationTextView);
+                destTextView.setText(destString);
+
+                destination = new Location("");
+                destination.setLatitude(destAddress.getLatitude());
+                destination.setLongitude(destAddress.getLongitude());
+
+                updateDistance();
+
+                Log.i("DestInfo", destAddress.toString());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateDistance() {
+        TextView distanceTextView = findViewById(R.id.distanceTextView);
+        String distanceString = String.format(Locale.getDefault(), "Distance: %.0f metres", currentLocation.distanceTo(destination));
+        distanceTextView.setText(distanceString);
+    }
+
+    private void toastMessage() {
+        Toast toast = Toast.makeText(getApplicationContext(),"It worked", Toast.LENGTH_SHORT);
+        toast.show();
+    }
 
 
 
